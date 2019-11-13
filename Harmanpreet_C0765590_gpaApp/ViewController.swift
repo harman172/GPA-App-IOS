211aceaecf03edc8478ea999d33bf118fb97f71f
Harman Kaur <kaurharmanpreet502@gmail.com>
2019-11-12 19:46:06 -0500

    Initial Commit

---
+++ Harmanpreet_C0765590_gpaApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Harmanpreet_C0765590_gpaApp
+//
+//  Created by MacStudent on 2019-11-12.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
